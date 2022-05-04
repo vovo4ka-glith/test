1e913cf250d4608f3ff5046690f0a6b5033b7dff
vovo4ka-glith <72129665+vovo4ka-glith@users.noreply.github.com>
2022-05-04 17:43:36 +0300

    Add files via upload

---
+++ test/test/Login/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  test
+//
+//  Created by Student on 04.05.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
---
+++ test/test/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingViewController.swift
+//  test
+//
+//  Created by Student on 04.05.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+}
